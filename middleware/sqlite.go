97312ea11a03bd0294dbb150068441153dcc8664
halushko <dgalun@gmail.com>
2025-12-30 20:48:30 +0200

    GetAllLevels API

--- middleware/sqlite.go
+++ middleware/sqlite.go
@@ -8,7 +8,7 @@ import (
 
 func GetAllLevels(db *utils.SQLite) ([]Level, error) {
 	levels := make([]Level, 0)
-	query := `SELECT name, lvl FROM levels ORDER BY level ASC;`
+	query := `SELECT name, lvl FROM levels ORDER BY lvl ASC;`
 	rows, err := db.Select(query)
 	if err != nil {
 		log.Errorf("GetAllLevels err: %v", err)
