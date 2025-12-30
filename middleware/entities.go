5d0a73f19bc275720181d3a81749fc582850c950
halushko <dgalun@gmail.com>
2025-12-30 21:09:57 +0200

    GetAllLevels API

--- middleware/entities.go
+++ middleware/entities.go
@@ -3,5 +3,5 @@ package middleware
 type Level struct {
 	ID    int    `json:"id,omitempty"`
 	Name  string `json:"name"`
-	Level int    `json:"level"`
+	Level int    `json:"level,omitempty"`
 }
--- middleware/sqlite.go
+++ middleware/sqlite.go
@@ -2,28 +2,27 @@ package middleware
 
 import (
 	"ist-syllabus-back/utils"
-	"strconv"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func GetAllLevels(db *utils.SQLite) ([]Level, error) {
 	levels := make([]Level, 0)
-	query := `SELECT DISTINCT name, lvl FROM levels ORDER BY CAST(lvl AS INTEGER) ASC;`
+	query := `SELECT DISTINCT name FROM levels ORDER BY CAST(lvl AS INTEGER) ASC;`
 	rows, err := db.Select(query)
 	if err != nil {
 		log.Errorf("GetAllLevels err: %v", err)
 		return nil, err
 	}
 	for _, row := range rows {
-		lvl, err := strconv.Atoi(row["lvl"].(string))
-		if err != nil {
-			log.Errorf("GetAllLevels Atoi err for Level column: %v", err)
-			return nil, err
-		}
+		//lvl, err := strconv.Atoi(row["lvl"].(string))
+		//if err != nil {
+		//	log.Errorf("GetAllLevels Atoi err for Level column: %v", err)
+		//	return nil, err
+		//}
 		level := Level{
-			Name:  row["name"].(string),
-			Level: lvl,
+			Name: row["name"].(string),
+			//Level: lvl,
 		}
 		levels = append(levels, level)
 	}
